b0e5811abfee2f3b6050c8610cc4140d0ea9db3a
shokhtalat <shokhrukhtalatov@gmail.com>
2024-08-14 10:21:55 +0900

    fix: Add views directory and update server.js

--- server.js
+++ server.js
@@ -16,13 +16,24 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4: Routing
+/*
 app.get("/hello", function(req, res) {
     res.end(`<h1 style="background: blue">HELLO WORLD by Tyler</h1>`);
 }); 
 app.get("/gift", function (req, res){
     res.end(`<h1>Siz sovg'alar bo'limidasiz!</h1>`)
 })
+*/
+app.post("/create-item", (req, res) =>{
+   // console.log(req.body);
+    //res.json({test: "success"});
+    // TODO: code with db here
+    
+});
 
+app.get('/', function (req, res) {
+    res.render("harid");
+})
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
