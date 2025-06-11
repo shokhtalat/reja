420afb1801383863f05cd9562d0d0fdebd07c2d6
shokhtalat <shokhrukhtalatov@gmail.com>
2025-06-11 10:14:35 +0900

    feat: create Dockerfile to generate images

--- server.js
+++ server.js
@@ -3,24 +3,29 @@ const http = require("http");
 const mongodb = require("mongodb");
 
 let db;
-const connectionString = "mongodb+srv://stalat:rHI0I9m5v08WNTAi@cluster0.movet.mongodb.net/Reja"
+const connectionString =
+  "mongodb+srv://stalat:rHI0I9m5v08WNTAi@cluster0.movet.mongodb.net/Reja";
 
-mongodb.connect(connectionString, {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true,},
-    (err, client) => {
-        if(err) console.log("Error on connection MongoDB");
-        else {
-            console.log("MongoDB connection succeed");
-            module.exports = client;
-            //console.log(client);
-            const app = require("./app");
-            const server = http.createServer(app);
-            let PORT = 7001;
-            server.listen(PORT, function () {
-                console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
-
-            });
-                    }
-    });
-
+mongodb.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("Error on connection MongoDB");
+    else {
+      console.log("MongoDB connection succeed");
+      module.exports = client;
+      //console.log(client);
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = process.env.PORT || 3000;
+      server.listen(PORT, function () {
+        console.log(
+          `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+        );
+      });
+    }
+  }
+);
